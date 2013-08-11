7f6b8109b1667bf1144eb4b8b9d0f03bbd84801b
Gray <gray@webpushers.com>
2013-08-11 03:01:22 -0700

    icons + hue animation & wallpaper loading

--- js/homescreen.js
+++ js/homescreen.js
@@ -1,14 +1,46 @@
 var Launcher = {};
+var wallpaper = wallpaper || {};
+wallpaper.options = wallpaper.options || {};
 
 (function(){
 	
-	function HomeScreen()
+	function HomeScreen(options)
 	{
+		options = options || {};
+		
 		this.initialize();
+		this.options = {
+			hue_animation : true
+		};
+		$.extend(this.options, options);
+		$.extend(this.options, wallpaper.options);
 	}
 	
 	HomeScreen.prototype.initialize = function()
 	{
+	}
+	
+	HomeScreen.prototype.applyOptions = function()
+	{
+		var background = this.background;
+		$.extend(this.options, wallpaper.options);
+		if (this.options.wallpaper != null)
+		{
+			background.setBackground(this.options.wallpaper);
+		}
+		this.update_hue();
+		
+		if (this.options.header_has_bg)
+		{
+			$('.modular-header').addClass('has-bg');
+		}
+		else
+		{
+			$('.modular-header').removeClass('has-bg');
+		}
+		
+		// hackery ??
+		if (this.options.hue_animation && !background.background.hasClass('hue-anim-end')) background.background.addClass('hue-anim-end'); 
 	}
 	
 	HomeScreen.prototype.start = function()
@@ -16,6 +48,33 @@ var Launcher = {};
 		console.log('this is the truth');
 		this.background = new Launcher.Background();
 		this.update_clock();
+		this.applyOptions();
+	}
+	
+	// should possibly be a function of background?
+	HomeScreen.prototype.update_hue = function(start)
+	{
+		var bg = this.background.background;
+		if (this.options.hue_animation)
+		{
+			if (!bg.hasClass('hue-anim')) bg.addClass('hue-anim');
+			if (bg.hasClass('hue-anim-end'))
+			{
+				console.log('hue animation reversing');
+				bg.removeClass('hue-anim-end');
+			}
+			else
+			{
+				console.log('hue animation starting');
+				if (!bg.hasClass('hue-anim-end')) {  bg.addClass('hue-anim-end'); } else { bg.removeClass('hue-anim-end'); } 
+			}
+			
+			setTimeout(this.update_hue.bind(this), 50 * 1000);
+		}
+		else
+		{
+			if (bg.hasClass('hue-anim')) bg.removeClass('hue-anim hue-anim-end');
+		}
 	}
 	
 	HomeScreen.prototype.update_clock = function()
@@ -44,9 +103,16 @@ var Launcher = {};
 			this.calcDimensions();
 		}
 		
+		pbg.prototype.setBackground = function(url)
+		{
+			this.background.css('background-image', 'url('+url+')');
+			this.calcDimensions();
+		}
+		
 		pbg.prototype.calcDimensions = function()
 		{
-			this.background_src = (($('.background').css('background-image').split("(")[1]).split(")")[0]);
+			this.background = $('.background');
+			this.background_src = ((this.background.css('background-image').split("(")[1]).split(")")[0]);
 			this.background_image = new Image();
 			this.background_image.onload = (function(){ this.background_width = this.background_image.width; this.background_height = this.background_image.height; this.calcDimensionsDone(); }).bind(this);
 			this.background_image.src = this.background_src;
@@ -56,6 +122,7 @@ var Launcher = {};
 		pbg.prototype.calcDimensionsDone = function()
 		{
 			console.log(' background image dimensions '+this.background_width+"x"+this.background_height);
+			this.scrollTo(1200);
 		}
 		
 		pbg.prototype.scrollTo = function(x)
