8823ee87d5f1cb3751c5baebdfe80328b451f97f
Gray <gray@webpushers.com>
2013-08-11 00:28:36 -0700

    css goin somewhere

--- js/homescreen.js
+++ js/homescreen.js
@@ -14,10 +14,70 @@ var Launcher = {};
 	HomeScreen.prototype.start = function()
 	{
 		console.log('this is the truth');
+		this.background = new Launcher.Background();
 	}
 	
+	var ParallaxBackground;
+	(function()
+	{
+		function pbg()
+		{
+			console.log('parallax background init');
+			this.calcDimensions();
+		}
+		
+		pbg.prototype.calcDimensions = function()
+		{
+			this.background_src = (($('.background').css('background-image').split("(")[1]).split(")")[0]);
+			this.background_image = new Image();
+			this.background_image.onload = (function(){ this.background_width = this.background_image.width; this.background_height = this.background_image.height; this.calcDimensionsDone(); }).bind(this);
+			this.background_image.src = this.background_src;
+		}
+		
+		// todo: promise api or something
+		pbg.prototype.calcDimensionsDone = function()
+		{
+			console.log(' background image dimensions '+this.background_width+"x"+this.background_height);
+		}
+		
+		pbg.prototype.scrollTo = function(x)
+		{
+			var y = 0;
+			$('.background').addClass('animated');
+			$('.background').css('background-position', '-'+x + 'px -' + y +'px');
+			setTimeout(function() { $('.background').removeClass('animated'); }, 1000);
+		}
+		
+		pbg.prototype.scrollImmediate = function(x)
+		{
+			$('.background').removeClass('animated'); 
+			var y = 0;
+			$('.background').css('background-position', '-'+x + 'px -' + y +'px');
+		}
+		
+		pbg.prototype.addBlur = function(px)
+		{
+			if (px == null) px = 2;
+			var value = $('.background').css('-webkit-filter');
+			if (value.indexOf("blur") != -1) { this.removeBlur(); this.addBlur(px); return; }
+			if (value == null) value = "";
+			value = value + " blur("+px+"px)";
+			$('.background').css('-webkit-filter', value);
+		}
+		
+		pbg.prototype.removeBlur = function()
+		{
+			var value = $('.background').css('-webkit-filter');
+			if (value != null && value != "") value = value.split('blur')[0];
+			$('.background').css('-webkit-filter', value);
+		}
+		
+		ParallaxBackground = pbg;
+	})();
+	
 	Launcher = {
-		HomeScreen: HomeScreen
+		HomeScreen: HomeScreen,
+		Background: ParallaxBackground
 	};
 	
 })();
